b3d1234cf1dc6e4a7c4fa0c1e184700f25c8cdf3
Colin <beutt102@mail.chapman.edu>
2023-08-28 12:29:45 -0700

    added Logo

--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -5,6 +5,7 @@ import "./Nav.css"
 function Nav(){
     return(
         <div className="navBar">
+            <a className="logo"></a>
             <NavLink className="navLink" to="/" activeClassName="active">Home</NavLink>
             <a className="node-separtor">/</a>
             <NavLink className="navLink" to="/code" activeClassName="active">Code</NavLink>
