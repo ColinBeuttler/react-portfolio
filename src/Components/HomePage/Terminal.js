67e348669c0ce2ea772b83ec828a5d4977e33b9a
Colin <beutt102@mail.chapman.edu>
2023-08-30 11:03:52 -0700

    adjusted panel sizing

--- src/Components/HomePage/Terminal.js
+++ src/Components/HomePage/Terminal.js
@@ -7,12 +7,12 @@ function Terminal() {
             <legend className="terminalLegend">🌌 about</legend>
             <a>-bash-3.2$ groups
                 unixuser feegroup figroup fogroup fumgroup</a>
-            <p>-self-Taught/Boot Camp Graduate with many projects</p>
-            <p>-experience with Mobile and Web Development with modern languages</p>
-            <p>-regularly updated GitHub and LinkedIn</p>
-            <p>-active on Social Media with quick responses</p>
-            <p>-available for Full and Part time work</p>
-            <a href="https://1drv.ms/w/s!AtgPspX-oSlPjLw6booJFFIk8R3N2A?e=5dbdQa" id="resume">-Resume Here</a>
+            <p>-self-taught/boot camp graduate with many projects</p>
+            <p>-experience with mobile and web development with modern languages</p>
+            <p>-regularly updated gitHub and linkedin</p>
+            <p>-active on social media with quick responses</p>
+            <p>-available for full and part time work</p>
+            <a href="https://1drv.ms/w/s!AtgPspX-oSlPjLw6booJFFIk8R3N2A?e=5dbdQa" id="resume">-resume here</a>
         </fieldset>
     )
 }
